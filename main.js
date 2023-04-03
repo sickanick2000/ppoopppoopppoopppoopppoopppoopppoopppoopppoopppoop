923b16444fabfe8aedb7196b4e3e88a546c10f15
sickanick2000 <105201537+sickanick2000@users.noreply.github.com>
2023-04-03 18:19:50 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("first console webapp")
+console.log(first_name)
+console.log(first_numba)
+console.log(sc_numba)
+console.log(a)
+console.log(fiaa)
+console.log(ppoop)
+
+
+var first_name = "jeffbob"
+
+var first_numba = "1"
+
+var sc_numba = "2"
+
+var a = "3"
+
+var fiaa = "4"
+
+var ppoop = "5"
